7c11192d6f89edee0af102a03197a343331e915c
omsutariya-ols <24bee114@spt.pdpu.ac.in>
2025-04-25 23:45:32 +0530

    Add files via upload

---
+++ LAB 09/L9Q8.py
@@ -0,0 +1,10 @@
+print("Name : OM SUTARIYA")
+print("Roll No. : 24BEE114")
+def convert(str1):
+    str1=input("Enter a string: ")
+    str1=str1.strip()
+    print(str1)
+
+
+user_input = ""
+convert(user_input) 
